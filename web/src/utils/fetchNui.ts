119364af09cac8f24dc2f314c19e28a2212c518c
Mojito-Fivem <liamdormon04@gmail.com>
2022-04-13 14:38:42 +0100

    docs(utils/fetchNui): add mockData param to jsdocs

--- web/src/utils/fetchNui.ts
+++ web/src/utils/fetchNui.ts
@@ -7,6 +7,7 @@ import { isEnvBrowser } from "./misc";
  *
  * @param eventName - The endpoint eventname to target
  * @param data - Data you wish to send in the NUI Callback
+ * @param mockData - Mock data to be returned if in the browser
  *
  * @return returnData - A promise for the data sent back by the NuiCallbacks CB argument
  */
